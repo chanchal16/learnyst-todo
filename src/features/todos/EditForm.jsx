052d5d61b4d5beb63e1ea53cf5bcce6f6eadbe67
chanchal <rajputchanchal17@gmail.com>
2023-01-30 00:05:18 +0530

    style - todo form

--- src/features/todos/EditForm.jsx
+++ src/features/todos/EditForm.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
 export const EditForm = ({currentTodo,
-    setIsEditing,
     onEditInputChange,
     onEditFormSubmit}) => {
   return (
-    <div>
-        <input type={'text'} value={currentTodo.todo} onChange={onEditInputChange} />
-        <button onClick={()=>onEditFormSubmit(currentTodo.id,currentTodo.todo,currentTodo.isCompleted)}>update</button>
-    </div>
+    <form onSubmit={(e)=>e.preventDefault()}>
+      <input type={'text'} className='input-field' value={currentTodo.todo} onChange={onEditInputChange} />
+      <button className='form-btn' onClick={()=>onEditFormSubmit(currentTodo.id,currentTodo.todo,currentTodo.isCompleted)}>
+        update</button>
+    </form>
   )
 }
--- src/features/todos/TodoForm.jsx
+++ src/features/todos/TodoForm.jsx
@@ -1,27 +1,32 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { ADD_TODO } from './todoSlice';
 import { v4 as uuidv4 } from 'uuid';
-
-export const TodoForm = () => {
-    const[todo,setTodo] = useState('');
-    const todosState = useSelector(state=>state.todos);
-    const todoDispatch = useDispatch()
-
-    const addTodo = () =>{
-        const todoObj = {
-            id:uuidv4(),
-            todo:todo,
-            isCompleted:false
-        }
-        todoDispatch(ADD_TODO(todoObj));
-        setTodo('')
-console.log('todos',todosState)
+ class TodoForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = ({
+        todo: "",
+    });
+  }
+  // add todo
+  addTodo = () =>{
+    const todoObj = {
+      id:uuidv4(),
+      todo:this.state.todo,
+      isCompleted:false
     }
-  return (
-    <div>
-        <input type={'text'} value={todo} onChange={(e)=>setTodo(e.target.value)} />
-        <button onClick={addTodo}>add</button>
-    </div>
-  )
+    this.props.ADD_TODO(todoObj);
+    this.setState({todo:''})
+  }
+  
+  render() {
+    return (
+      <form onSubmit={(e)=>e.preventDefault()} >
+        <input type={'text'} className='input-field' value={this.state.todo} onChange={(e)=>this.setState({todo:e.target.value})} />
+        <button onClick={this.addTodo} className='form-btn'>Add</button>
+      </form>
+    )
+  }
 }
+export default connect(null,{ADD_TODO})(TodoForm);
