e050fab91b6951bcfa2855626a4036daedf82404
chanchal <rajputchanchal17@gmail.com>
2023-01-30 00:08:28 +0530

    chore - remove commented code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import './App.css'
-import { TodoList } from './components/TodoList'
-import { TodoForm } from './features/todos/TodoForm'
 import { Todos } from './pages/Todos'
 
 function App() {
   return (
     <div className="App">
-      {/* <TodoForm/>
-      <TodoList/> */}
       <Todos/>
     </div>
   )
